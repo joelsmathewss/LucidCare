06bcf961695c8e680ae1cdf919ad83c6d5605d36
Joel S Mathews <joelsmathewsj@Joels-MacBook-Air.local>
2025-08-28 21:32:18 +0530

    Add tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+// tailwind.config.cjs (or .js if using ESM syntax)
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
