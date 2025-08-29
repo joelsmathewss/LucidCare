57ae95e7e7d4673e547acf6158232dba24c57f26
Joel S Mathews <joelsmathewsj@Joels-MacBook-Air.local>
2025-08-29 06:42:43 +0530

    Integrate Signup and Login routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import FeaturesSection from "./components/FeaturesSection";
-import HowItWorksSection from "./components/HowItWorksSection"; 
+import HowItWorksSection from "./components/HowItWorksSection";
 import CTASection from "./components/CTASection";
 import Footer from "./components/Footer";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <FeaturesSection />
+      <HowItWorksSection />
+      <CTASection />
+      <Footer />
+    </>
+  );
+}
 
 export default function App() {
   return (
-    <div className="bg-white text-gray-800 font-inter">
-      <Navbar />
-      <main>
-        <HeroSection />
-        <FeaturesSection />
-        <HowItWorksSection />
-        <CTASection />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="bg-white text-gray-800 font-inter">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
